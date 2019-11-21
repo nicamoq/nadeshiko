a39268f2cfbb2dc5ba6cfc97d086955e0e02e744
nicamoq <hsudoru@gmail.com>
2019-11-21 23:32:31 +0800

    Update sources.bat

--- sources.bat
+++ sources.bat
@@ -4,17 +4,17 @@
 :: that contains spaces ex. C:\Videos\"Yuru Camp"\"Yuru Camp Episode 1.mkv"
 
 
-set sc1=D:\MEMORY4GBACKUP\ANIME\ALLDEMGOODSTUFF-KUTSUROSD\Anime-CDROM\"Yuru Camp"\"[AnimeOut] Yuru Camp - 01 [720p][Asenshi][Frenzy].mkv"
-set sc2=D:\MEMORY4GBACKUP\ANIME\ALLDEMGOODSTUFF-KUTSUROSD\Anime-CDROM\"Yuru Camp"\"[AnimeOut] Yuru Camp - 02 [720p][Asenshi][Frenzy].mkv"
-set sc3=D:\MEMORY4GBACKUP\ANIME\ALLDEMGOODSTUFF-KUTSUROSD\Anime-CDROM\"Yuru Camp"\"[AnimeOut] Yuru Camp - 03 [720p][Asenshi][Frenzy].mkv"
-set sc4=D:\MEMORY4GBACKUP\ANIME\ALLDEMGOODSTUFF-KUTSUROSD\Anime-CDROM\"Yuru Camp"\"[AnimeOut] Yuru Camp - 04 [720p][Asenshi][Frenzy].mkv"
-set sc5=D:\MEMORY4GBACKUP\ANIME\ALLDEMGOODSTUFF-KUTSUROSD\Anime-CDROM\"Yuru Camp"\"[AnimeOut] Yuru Camp - 05 [720p][Asenshi][Frenzy].mkv"
-set sc6=D:\MEMORY4GBACKUP\ANIME\ALLDEMGOODSTUFF-KUTSUROSD\Anime-CDROM\"Yuru Camp"\"[AnimeOut] Yuru Camp - 06 [720p][Asenshi][Frenzy].mkv"
-set sc7=D:\MEMORY4GBACKUP\ANIME\ALLDEMGOODSTUFF-KUTSUROSD\Anime-CDROM\"Yuru Camp"\"[AnimeOut] Yuru Camp - 07 [720p][Asenshi][Frenzy].mkv"
-set sc8=D:\MEMORY4GBACKUP\ANIME\ALLDEMGOODSTUFF-KUTSUROSD\Anime-CDROM\"Yuru Camp"\"[AnimeOut] Yuru Camp - 08 [720p][Asenshi][Frenzy].mkv"
-set sc9=D:\MEMORY4GBACKUP\ANIME\ALLDEMGOODSTUFF-KUTSUROSD\Anime-CDROM\"Yuru Camp"\"[AnimeOut] Yuru Camp - 09 [720p][Asenshi][Frenzy].mkv"
-set sc10=D:\MEMORY4GBACKUP\ANIME\ALLDEMGOODSTUFF-KUTSUROSD\Anime-CDROM\"Yuru Camp"\"[AnimeOut] Yuru Camp - 10 [720p][Asenshi][Frenzy].mkv"
-set sc11=D:\MEMORY4GBACKUP\ANIME\ALLDEMGOODSTUFF-KUTSUROSD\Anime-CDROM\"Yuru Camp"\"[AnimeOut] Yuru Camp - 11 [720p][Asenshi][Frenzy].mkv"
-set sc12=D:\MEMORY4GBACKUP\ANIME\ALLDEMGOODSTUFF-KUTSUROSD\Anime-CDROM\"Yuru Camp"\"[AnimeOut] Yuru Camp - 12 [720p][Asenshi][Frenzy].mkv"
+set sc1=
+set sc2=
+set sc3=
+set sc4=
+set sc5=
+set sc6=
+set sc7=
+set sc8=
+set sc9=
+set sc10=
+set sc11=
+set sc12=
 
 exit /b
